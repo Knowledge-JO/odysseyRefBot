d9594844d5cfea1558b4aa9f2942bac5fd60a3dc
Knowledge-JO <megamindtheincredible@gmail.com>
2024-01-04 01:55:54 +0100

    bot update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import TelegramBot, { Message } from "node-telegram-bot-api";
-
+import TelegramBot, { KeyboardButton, Message } from "node-telegram-bot-api";
+import dotenv from "dotenv"
+dotenv.config()
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '6334819793:AAGAJn3ZRbrgUEhcFU9KQrI_pZFsruOgUek';
+const token = process.env.TELEGRAM_BOT_TOKEN || "";
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -11,7 +12,22 @@ type RefData = {
     referrals: Array<number>
 }
 
+const keyboard: KeyboardButton[][] = [
+   [{text: "referrals"}, {text: "referral link"}]
+]
+
 const DB: RefData[] = []
+
+bot.onText(/\/start/, (msg: Message) => {
+    if(msg.chat.type !== "private") return
+    const chatId = msg.chat.id
+    bot.sendMessage(chatId, "Welcome to the Odyssey referral bot", {
+        reply_markup: {
+            keyboard,
+            resize_keyboard: true
+        }
+    })
+})
 
 bot.onText(/\/start (.+)/ , (msg: Message, match) => {
     if(msg.chat.type !== "private") return
@@ -24,15 +40,29 @@ bot.onText(/\/start (.+)/ , (msg: Message, match) => {
         updateDB(referringUserId, chatId)
         console.log(DB)
     }
+    bot.sendMessage(chatId, "Welcome to the Odyssey referral bot", {
+        reply_markup: {
+            keyboard,
+            resize_keyboard: true
+        }
+    })
 })
 
-bot.onText(/\/referrals/, (msg: Message) => {
+bot.onText(/referrals/, (msg: Message) => {
     if(msg.chat.type !== "private") return
     const chatId = msg.chat.id
     const index = getIndex(chatId.toString())
     const data = DB[index]
     const totalRefs = data ? data.referrals.length : 0
     const message = `<b>Total - ${totalRefs}</b>`
+    bot.sendMessage(chatId, message, {parse_mode: "HTML"})
+})
+
+
+bot.onText(/referral link/, (msg: Message) => {
+    if(msg.chat.type !== "private") return
+    const chatId = msg.chat.id
+    const message = `<b>Your ref link: https://t.me/odysseyref_bot?start=${chatId}</b>`
     bot.sendMessage(chatId, message, {parse_mode: "HTML"})
 })
 
